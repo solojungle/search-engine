a275db658cf7708d3091d9a1442695620b18b546
Ali Awari <solojungle@users.noreply.github.com>
2023-08-25 15:59:15 -0400

    feat: changed results video, removed url

--- src/components/Results/Video/item.tsx
+++ src/components/Results/Video/item.tsx
@@ -9,22 +9,21 @@ export const Item = (props: ResultProps) => {
   const { title, description, url, thumbnail } = props;
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-4">
       <div className="relative h-56 w-full overflow-hidden rounded-md">
-        <img
-          className="absolute h-full w-full object-cover"
-          src={thumbnail}
-          alt={title}
-        />
+        <a href={url} className="">
+          <img
+            className="absolute h-full w-full object-cover"
+            src={thumbnail}
+            alt={title}
+          />
+        </a>
       </div>
       <div className="flex flex-col gap-1">
         <a className="text-lg font-bold" href={url}>
           {title}
         </a>
         <p className="text-sm">{description}</p>
-        <a className="text-sm text-blue-500" href={url}>
-          {url}
-        </a>
       </div>
     </div>
   );
